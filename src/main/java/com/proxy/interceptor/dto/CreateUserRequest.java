02839ca72ad5291660cf17e3a06577ba4f74343a
gautamrajesh007 <gautamrdev7@gmail.com>
2026-01-29 13:09:02 +0530

    add CreateUserRequest record for handling user creation data

---
+++ src/main/java/com/proxy/interceptor/dto/CreateUserRequest.java
@@ -0,0 +1,9 @@
+package com.proxy.interceptor.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateUserRequest(
+        @NotBlank String username,
+        @NotBlank String password,
+        @NotBlank String role
+) {}
